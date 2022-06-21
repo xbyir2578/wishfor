454ffc59d658ce56b7c237169f21d1b35f354cb3
yuria imaizumi <xbyir2578@outlook.jp>
2022-06-21 06:13:45 +0000

    user page

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $user = User::orderBy('id', 'derc')->paginate(10);
+        
+        return view('users.index', [
+           'users' => $users, 
+        ]);
+    }
+    
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
+}
